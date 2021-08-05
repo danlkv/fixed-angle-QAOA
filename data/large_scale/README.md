59927a149a4893321053d051f8ac52c75f6fe317
Danil Lykov <lkv97dn@gmail.com>
2021-08-05 12:22:22 -0500

    add large scale tw calc

---
+++ data/large_scale/README.md
@@ -0,0 +1,13 @@
+# Treewidth for larger connectivity
+
+conn = 4
+
+p = 5, width = 10
+p = 6, width = 12, nodes=2186
+p = 7, width = , nodes=6560
+
+
+conn = 5
+
+p = 5, width = 10
+p = 6, width = 10
---
+++ data/large_scale/calc_tw.py
@@ -0,0 +1,54 @@
+import qtensor
+from collections import defaultdict
+import networkx as nx
+
+p = 7
+deg = 4
+def bethe_graph(p, degree):
+    def add_nodes_to_leafs(graph, deg=3):
+        """ Works in-place """
+        leaves = [n for n in graph.nodes() if graph.degree(n) <= 1]
+        n = graph.number_of_nodes()
+        for leaf in leaves:
+            next_edges = [(leaf, n+x) for x in range(1, deg)]
+            graph.add_edges_from(next_edges)
+            n += deg-1
+    graph = nx.Graph()
+    graph.add_edges_from([(0,1)])
+    for i in range(p):
+        add_nodes_to_leafs(graph, deg=degree)
+    n = graph.number_of_nodes()
+    assert n == 2*sum([(deg-1)**y for y in range(p+1)])
+    return graph
+
+graph = bethe_graph(p, degree=deg)
+print(f'Graph has {graph.number_of_nodes()} nodes')
+stats = graph.degree
+x = defaultdict(int, {})
+for node, deg in stats:
+    x[deg] += 1
+
+print(f'Graph degrees stats: {x}')
+
+
+from qtensor.optimisation.Optimizer import TamakiOptimizer
+from qtensor.optimisation import QtreeTensorNet
+def optimize_lightcone(self, G, p, edge):
+    """
+    Builds a circuit for corresponding edge, optimizes it and caches the result
+    """
+    gamma, beta = [.1]*p, [.2]*p
+    circuit = self._edge_energy_circuit(G, gamma, beta, edge=edge)
+
+    tn = QtreeTensorNet.from_qtree_gates(circuit, backend=self.backend)
+    peo, tn = self.optimizer.optimize(tn)
+    width = self.optimizer.treewidth
+    # debt: proper opt data needed
+    return peo, width
+
+opt = TamakiOptimizer(wait_time=200)
+sim = qtensor.QAOAQtreeSimulator(qtensor.DefaultQAOAComposer, optimizer=opt)
+peo, width  = optimize_lightcone(sim, graph, p, edge=(0, 1))
+
+print('peo is ', peo)
+print('width is', width)
